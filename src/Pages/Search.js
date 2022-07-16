4a4039b1f4f693aa889fc0000bfdc182d9fc9c17
Aron Adams <aronrapetto@hotmail.com>
2022-07-15 21:36:41 -0300

    renderizando receitas através do estado do context, didmount de receitas aplicado, footer renderizado

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,22 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../Components/Header';
+import RecipeCard from '../Components/RecipeCards';
+import RecipesContext from '../Context/recipesContext';
+import Footer from '../Components/Footer';
+import fetchAPIs from '../services/FetchAPI';
 
 function Search({ history: { location: { pathname } } }) {
   const [recipeType, setRecipeType] = useState('');
 
+  const { recipes, setRecipes } = useContext(RecipesContext);
+
   useEffect(() => {
-    if (pathname === '/foods') {
-      setRecipeType('Foods');
-    }
-    if (pathname === '/drinks') {
-      setRecipeType('Drinks');
-    }
-  }, [pathname]);
+    const setInitalRecipes = async () => {
+      if (pathname === '/foods') {
+        setRecipeType('Foods');
+        const data = await fetchAPIs('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+        const responseRecipes = [...Object.values(data)[0]];
+        setRecipes(responseRecipes);
+      }
+      if (pathname === '/drinks') {
+        setRecipeType('Drinks');
+        const data = await fetchAPIs('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+        const responseRecipes = [...Object.values(data)[0]];
+        setRecipes(responseRecipes);
+      }
+    };
+    setInitalRecipes();
+  }, [pathname, setRecipes]);
+
+  const MAX_LENGTH_CARDS = 12;
 
   return (
     <main>
       <Header headerText={ recipeType } isSearchPage />
+      <section>
+        {
+          recipes.length > 0 && recipes.map((recipe, index) => {
+            if (index < MAX_LENGTH_CARDS) {
+              return (
+                <RecipeCard
+                  key={ recipeType === 'Foods' ? recipe.idMeal : recipe.idDrink }
+                  index={ index }
+                  recipeId={ recipeType === 'Foods' ? recipe.idMeal : recipe.idDrink }
+                  recipeImage={ recipeType === 'Foods'
+                    ? recipe.strMealThumb : recipe.strDrinkThumb }
+                  recipeName={ recipeType === 'Foods' ? recipe.strMeal : recipe.strDrink }
+                  recipeType={ recipeType }
+                />
+              );
+            }
+            return null;
+          })
+        }
+      </section>
+      <Footer />
     </main>
   );
 }
