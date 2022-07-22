493d67a97e75154e6c4bc3f40ed465e0d78e7ca5
Aron Adams <aronrapetto@hotmail.com>
2022-07-21 23:25:49 -0300

    problema com o mock do searchBar

--- src/tests/searchBar.test.js
+++ src/tests/searchBar.test.js
@@ -3,63 +3,58 @@ import { screen, waitFor } from '@testing-library/react';
 import renderWithRouter from './helper/renderWithRouter';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
-import mealsByIngredient from './mocks/mockForIngredients';
-import drinks from './mocks/mockDrinks';
-import FetchAPIs from '../services/FetchAPI';
+import mealsByIngredient from './mocks/mealsByIngredients';
+import drinks from './mocks/drinks';
+import mealCategories from './mocks/mealCategorys';
 // import { mockComponent } from 'react-dom/test-utils';
 
 describe('Testa a barra de busca na página de comidas', () => {
   beforeEach(() => {
-  //   jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
-  //     json: () => Promise.resolve(mealsByIngredient),
-  //   }))
-    FetchAPIs = jest.fn().mockImplementation(() => Promise.resolve(mealsByIngredient));
+    jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mealsByIngredient),
+    }));
   });
  
   afterEach(() => jest.clearAllMocks());
   
-  // it('Verifica a funcionalidade do filtro de ingredientes', async () => {
-  //   const { history } = renderWithRouter(<App />);
-  //   history.push('/foods');
-
-  //   const openSearchBtn = screen.getByTestId('search-top-btn');
-  //   userEvent.click(openSearchBtn);
-
-  //   const search = screen.getByTestId('search-input');
-  //   const radio1 = screen.getByTestId('ingredient-search-radio');
-  //   const searchButton = screen.getAllByRole('button', { name:/search/i });
-
-  //   userEvent.click(radio1);
-  //   userEvent.type(search, 'chicken');
-  //   userEvent.click(searchButton[1]);
-
-  //   await waitFor(() => expect(global.fetch).toHaveBeenCalled());
-
-  //   const ingredient = screen.getByTestId('0-card-name');
-  //   expect(ingredient).toHaveTextContent('Brown Stew Chicken');
-  // });
-
-  it('Verifica a funcionalidade do filtro de nome', async () => {
-    // jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({
-    //   json: () => Promise.resolve(drinks),
-    // }));
-
+  it('Verifica a funcionalidade do filtro de ingredientes', async () => {
     const { history } = renderWithRouter(<App />);
     history.push('/foods');
 
-    await waitFor(() => expect(FetchAPIs()).toHaveBeenCalledTimes(2));
+    const openSearchBtn = screen.getByTestId('search-top-btn');
+    userEvent.click(openSearchBtn);
 
-    // jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
-    //   json: () => Promise.resolve({
-    //     'meals': [
-    //       {
-    //         'strMeal': 'Brown Stew Chicken',
-    //         'strMealThumb': 'https://www.themealdb.com/images/media/meals/sypxpx1515365095.jpg',
-    //         'idMeal': '52940'
-    //       }
-    //     ]
-    //   }),
-    // }));
+    const search = screen.getByTestId('search-input');
+    const radio1 = screen.getByTestId('ingredient-search-radio');
+    const searchButton = screen.getAllByRole('button', { name:/search/i });
+
+    userEvent.click(radio1);
+    userEvent.type(search, 'chicken');
+    userEvent.click(searchButton[1]);
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+
+    const ingredient = screen.getByTestId('0-card-name');
+    expect(ingredient).toHaveTextContent('Brown Stew Chicken');
+  });
+
+  it('Verifica a funcionalidade do filtro de nome', async () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/foods');
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(2));
+
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
+      json: () => Promise.resolve({
+        'meals': [
+          {
+            'strMeal': 'Brown Stew Chicken',
+            'strMealThumb': 'https://www.themealdb.com/images/media/meals/sypxpx1515365095.jpg',
+            'idMeal': '52940'
+          }
+        ]
+      }),
+    }));
 
     const openSearchBtn = screen.getByTestId('search-top-btn');
     userEvent.click(openSearchBtn);
@@ -73,7 +68,9 @@ describe('Testa a barra de busca na página de comidas', () => {
     userEvent.click(searchButton);
     
     // await waitFor(() => expect(history.location.pathname).toBe('/foods/52940'));
-    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
+    await waitFor(() => expect(global.fetch).toHaveBeenCalled());    
+
+    // Problema de reconhecimento dos elementos corrigido, não faço idéia de como mockar a resposta dos drinks recomendados no meio de 3 fetchs diferentes
 
     expect(history.location.pathname).toBe('/foods/52940')
 
